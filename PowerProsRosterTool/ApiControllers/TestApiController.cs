2b00706edc1fa78106a9f8f689b67cb108f879bb
Chris Shorter <shorte24@gmail.com>
2020-10-11 11:10:49 -0400

    Setting up Power Pros Roster Tool Repository, Database, and first Api Controller

---
+++ PowerProsRosterTool/ApiControllers/TestApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerProsRosterTool.ApiControllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+
+        public TestApiController() { }
+
+        [HttpGet("get-data")]
+        public TestResponse GetData()
+        {
+            var context = new PowerProsDbContext();
+            var player = context.Player.First();
+            return TestResponse.ForPlayer(player);
+        }
+    }
+
+    public class TestResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public static TestResponse ForPlayer(Player player)
+        {
+            return new TestResponse() { 
+                Id = player.Id,
+                Name = player.Name,
+                Number = player.Number
+            };
+        }
+    }
+}
---
+++ PowerProsRosterTool/PowerProsDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerProsRosterTool
+{
+    public class PowerProsDbContext : DbContext
+    {
+        public DbSet<Player> Player { get; set; }
+
+        public PowerProsDbContext(DbContextOptions<PowerProsDbContext> options)
+        : base(options)
+        { }
+
+        public PowerProsDbContext()
+        : base()
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseMySQL("server=localhost;database=pp;user=root;password=MlbPowerPros");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>();
+        }
+    }
+
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
